a1fe302862eca5721fcae9d0bf1bc98c1d88ae6b
Rodrigo <rodrigodhv@gmail.com>
2024-09-17 19:51:49 +0200

    Seccion 4

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,7 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+  public characters: Character[] = [
+    {
+      name: 'Krilin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      name: 'Vegeta',
+      power: 7500,
+    },
+  ];
+
+  onNewCharacter(character: Character): void {
+    // debugger;
+
+    console.log('Main Page');
+    console.log({ character });
+    this.characters.push(character);
+  }
+
+  onDelete(index: number): void {
+    // debugger;
+
+    console.log('Main Page');
+    console.log({ index });
+    this.characters.splice(index, 1);
+  }
+}

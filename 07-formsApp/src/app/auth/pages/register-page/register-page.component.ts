5a11726dce71101881c9378d8f2617e254e4d80f
Rodrigo <rodrigodhv@gmail.com>
2024-10-20 19:15:19 +0200

    Sección 18

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,61 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import * as customValidators from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/service/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            this.validatorsService.firstNameAndLastnamePattern
+          ),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorsService.emailPattern),
+        ],
+        [this.emailValidator],
+      ],
+      username: [
+        '',
+        [Validators.required, this.validatorsService.cantBeStrider],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        //esto está lanzando el deprecated del group
+        this.validatorsService.isFieldOneEqualsFieldTwo(
+          'password',
+          'password2'
+        ),
+      ],
+    }
+  );
 
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidatorService
+  ) {}
+
+  isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+
+  onSumit() {
+    this.myForm.markAllAsTouched();
+  }
 }

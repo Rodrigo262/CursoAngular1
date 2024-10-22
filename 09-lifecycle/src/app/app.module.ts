4c7f565a27dbfbbb42c0736613a7c88809e98f93
Rodrigo <rodrigodhv@gmail.com>
2024-10-22 17:36:32 +0200

    Sección 20

---
+++ 09-lifecycle/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsModule } from './products/products.module';
+import { ProductComponent } from './products/pages/product/product.component';
+import { PriceComponent } from './products/components/price/price.component';
+
+@NgModule({
+  declarations: [AppComponent, ProductComponent, PriceComponent],
+  imports: [BrowserModule, AppRoutingModule, ProductsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}

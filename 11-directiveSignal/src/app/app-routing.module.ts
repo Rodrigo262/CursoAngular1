4e14de2e2c352a9be12946322fd3b2bd95aeb833
Rodrigo <rodrigodhv@gmail.com>
2024-11-04 18:14:52 +0100

    Sección 23

---
+++ 11-directiveSignal/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then((m) => m.ProductsModule),
+  },
+  { path: 'products', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) },
+  {
+    path: '**',
+    redirectTo: 'products',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes), ReactiveFormsModule],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
